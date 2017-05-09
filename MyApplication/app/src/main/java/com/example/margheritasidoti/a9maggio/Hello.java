edf7db21f054bb3a214a3aefd6c1ea18b144910c
msidoti <margherita.sidoti@eng.it>
2017-05-09 15:50:24 +0200

    hello class

---
+++ MyApplication/app/src/main/java/com/example/margheritasidoti/a9maggio/Hello.java
@@ -0,0 +1,12 @@
+package com.example.margheritasidoti.a9maggio;
+
+/**
+ * Created by margheritasidoti on 09/05/17.
+ */
+
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("HELLO");
+    }
+}
--- MyApplication/app/src/main/java/com/example/margheritasidoti/a9maggio/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/margheritasidoti/a9maggio/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +16,14 @@ public class MainActivity extends AppCompatActivity {
         Log.i("MAIN", "SET");
         Log.i("MAIN", "SET 2");
         Log.i("MAIN", "SET 3");
-        Intent i = new Intent(MainActivity.this, SecondActivity.class);
-        startActivity(i);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
