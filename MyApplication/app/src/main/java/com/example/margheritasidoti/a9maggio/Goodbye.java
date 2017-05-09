f0ea68261d8f0b82397aa471861734367219631a
msidoti <margherita.sidoti@eng.it>
2017-05-09 16:12:04 +0200

    goodbye

---
+++ MyApplication/app/src/main/java/com/example/margheritasidoti/a9maggio/Goodbye.java
@@ -0,0 +1,11 @@
+package com.example.margheritasidoti.a9maggio;
+
+/**
+ * Created by margheritasidoti on 09/05/17.
+ */
+
+public class Goodbye {
+    public static void main(String[] args){
+        System.out.println("Goodbye");
+    }
+}
