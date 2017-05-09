294dfcc18a0da5e94e6cbd92094f3495ea4cca81
msidoti <margherita.sidoti@eng.it>
2017-05-09 16:07:31 +0200

    hello class system bye

--- MyApplication/app/src/main/java/com/example/margheritasidoti/a9maggio/Hello.java
+++ MyApplication/app/src/main/java/com/example/margheritasidoti/a9maggio/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("HELLO");
         System.out.println("BU");
+        System.out.println("Bye");
     }
 }
