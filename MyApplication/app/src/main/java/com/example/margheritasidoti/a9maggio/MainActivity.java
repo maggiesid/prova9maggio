14343e0f3685298c7144440fa3acf9194c126235
msidoti <margherita.sidoti@eng.it>
2017-05-30 12:58:22 +0200

    log 11 da task tracked 2

--- MyApplication/app/src/main/java/com/example/margheritasidoti/a9maggio/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/margheritasidoti/a9maggio/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         Log.i("MAIN", "develop-1 8");
         Log.i("MAIN", "task tracked 9");
         Log.i("MAIN", "task tracked 10");
+        Log.i("MAIN", "task tracked 2  11");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
