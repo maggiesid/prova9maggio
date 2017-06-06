dee6c3c3663825fb6e7629d69ec9a3296879243b
msidoti <margherita.sidoti@eng.it>
2017-06-06 09:09:43 +0200

    nel bottone

--- MyApplication/app/src/main/java/com/example/margheritasidoti/a9maggio/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/margheritasidoti/a9maggio/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(i);
+                Log.i("MAIN", "6 giugno dal primo");
             }
         });
 
